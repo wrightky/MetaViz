be1460f3d9e9401cb226a8ef359ad62d51537973
Kyle Wright <30955855+wrightky@users.noreply.github.com>
2020-12-23 15:06:09 -0600

    Create Collection.py

---
+++ MediaArchive/Collection.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Establish main class for the photo archive/collection
+"""
+import numpy as np
+import os
+import pandas
+import subprocess
+import re
+import .config as cf
+
+class Collection():
+    """Parent class for the media collection"""
+    def __init__(self):
+        # Store details on collection location
+        self.CollectionPath = cf.CollectionPath
+        self.csvPath = cf.csvPath
+        self.subfolders = cf.subfolders
+        
+        # Information on packages
+        self.SeabornAvailable = cf.SeabornAvailable
+        self.ChordAvailable = cf.ChordAvailable
+        self.NetworkXAvailable = cf.NetworkXAvailable
