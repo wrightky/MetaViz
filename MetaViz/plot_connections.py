eddf2822fdc6720da5187c636e5c3e1826c19a20
wrightky <kyleawright@utexas.edu>
2021-02-18 17:34:00 -0600

    add inside-field keyword heatmap

--- MetaViz/plot_connections.py
+++ MetaViz/plot_connections.py
@@ -58,3 +58,62 @@ def ChordChart(Archive, keywords, fields):
     # Plot
     Chord(matrix, keywords, width=600).show()
     return
+
+
+def Heatmap1(Archive, field, N=20, exclude=None, include=None):
+    """
+    For the top N keywords in field, plot a heatmap showing the
+    extent to which those keywords tend to appear along with
+    other keywords inside the SAME metadata field.
+    
+    Differs from Heatmap2, which looks for connections BETWEEN fields.
+    
+    Inputs:
+        Archive (obj) : archive.Archive() object used to access
+            metadata fields via FindSource()
+        field (str) : Field in which to look for keywords
+        N (int) : Number of entries to show on the chart
+        exclude (list or bool) : List of entries in data to
+            specifically exclude from the plot
+        include (list or bool) : List of entries in data to
+            specifically include in the plot
+    """
+    # Get top entries in field
+    data = Archive.GrabData(None, [field])
+    data = tools.CountUnique(data[field], delimiter=', ')
+    x = data['Entry'].tolist()
+
+    # Filter the data
+    if exclude is not None:
+        for exc in exclude:
+            x = [x[i] for i in list(range(len(x))) if exc not in x[i]]
+    if include is not None:
+        x = [x[i] for i in list(range(len(x))) if x[i] in include]
+    keywords = x[0:N]
+
+    # Initialize matrix of intersection counts
+    matrix = np.zeros((len(keywords),len(keywords)), dtype=float)
+
+    # Loop through and find intersection counts
+    for ii, name_A in enumerate(keywords):
+        for jj, name_B in enumerate(keywords):
+            if jj > ii:
+                A = Archive.FindSource([name_A], [field])
+                B = Archive.FindSource([name_B], [field])
+                AB = tools.IntersectLists([A, B])
+                matrix[ii, jj] = len(AB)/(len(A)+len(B)-len(AB))
+
+    # Make symmetric:
+    matrix = (matrix + matrix.T - np.diag(np.diag(matrix))).tolist()
+
+    # Do plotting
+    fig = plt.figure(figsize=(6,6), dpi=200)
+    plt.imshow(matrix, cmap='CMRmap')
+    ax = plt.gca()
+    ax.set_yticks(list(np.arange(0, N)))
+    b = ax.set_yticklabels(keywords)
+    ax.set_xticks(list(np.arange(0, N)))
+    b = ax.set_xticklabels(keywords, rotation=-90)
+    c = plt.colorbar(fraction=0.045)
+    ax.set_title('Keyword Correlation in %s' % field)
+    return
